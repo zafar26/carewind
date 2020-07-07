650088e771b608649c2775be386872e3f9f1f317
ahmed <amamudassir@gmail.com>
2020-07-07 17:16:12 +0530

    add icons to the tabs

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import {
   IonTabs
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { time, people, pieChart } from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
@@ -34,6 +34,12 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+const tabNames = {
+  tab1 : "Bookings",
+  tab2 : "Patients",
+  tab3 : "Reports"
+}
+
 const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
@@ -46,16 +52,16 @@ const App: React.FC = () => (
         </IonRouterOutlet>
         <IonTabBar slot="bottom">
           <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
+            <IonIcon icon={time} />
+            <IonLabel>{tabNames.tab1}</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
+            <IonIcon icon={people} />
+            <IonLabel>{tabNames.tab2}</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon icon={square} />
-            <IonLabel>Tab 3</IonLabel>
+            <IonIcon icon={pieChart} />
+            <IonLabel>{tabNames.tab3}</IonLabel>
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
