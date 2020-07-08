b30844b8c5a9a759e8d1e4e7c2fc8765ff88876d
zafar26 <qutubuddinzafar2616118@gmail.com>
2020-07-08 18:30:12 +0530

    renamed d as patient

--- src/pages/bookings.tsx
+++ src/pages/bookings.tsx
@@ -1,28 +1,21 @@
 import React from 'react';
-import { IonContent, IonPage, } from '@ionic/react';
-import {  IonList  } from '@ionic/react';
-import Card from "../components/Card"
+import { IonContent, IonPage } from '@ionic/react';
+import { IonList } from '@ionic/react';
+import Card from "../components/Card";
 import './bookings.css';
-import Header from '../components/Header'
+import Header from '../components/Header';
+import { Patients } from '../utils/patients';
 
-const patientArray = [
-  { name: "Khaja Naquiuddin", age:32, days: 3, status:"Done" },
-  { name: "Mohammed Zafar", age:22, days: 2, status:"Done" },
-  { name: "M.A Mudassir", age:22, days: 1, status:"Done" },
-  { name: "Khaja Naquiuddin", age:32, days: 3, status:"Cancelled" },
-]
 
-function Bookings() {
+export default  function Bookings() {
   return (
     <IonPage>
       <Header />
       <IonContent>
         <IonList>
-          { patientArray.map((d) => <Card name={d.name} age={d.age} days={d.days} status={d.status} />)}
+          {Patients.map((patient) => <Card name={patient.name} age={patient.age} days={patient.days} status={patient.status} />)}
         </IonList>
       </IonContent>
     </IonPage>
   );
 }
-
-export default Bookings;
