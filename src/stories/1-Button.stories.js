904518022560d1b4ec228f4381e1b5d2fc746172
zafar26 <qutubuddinzafar2616118@gmail.com>
2020-07-09 21:59:35 +0530

    story book button Added

--- src/stories/1-Button.stories.js
+++ src/stories/1-Button.stories.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
-import { Button } from '@storybook/react/demo';
+import Button from "../components/Button"
 
 export default {
   title: 'Button',
   component: Button,
 };
 
-export const Text = () => <Button onClick={action('clicked')}>Hello Button</Button>;
-
-// export const Emoji = () => (
-//   <Button onClick={action('clicked')}>
-//     <span role="img" aria-label="so cool">
-//       😀 😎 👍 💯
-//     </span>
-//   </Button>
-// );
+export const Button_new_test = () => <Button label="V" type="start" time="time" onClick={action('clicked Button')}>Hello Button</Button>;
+// export const Button_new_test = () => <Button label="cancel" type="cancel" onClick={action("CLICKED")}>Hello Button</Button>;
